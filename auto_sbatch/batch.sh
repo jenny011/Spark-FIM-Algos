28bfe36f5df3a9358e8a6177de4d4cad128572b0
jenny011 <jz2915@nyu.edu>
2021-06-30 06:34:17 +0800

    tuned

--- auto_sbatch/batch.sh
+++ auto_sbatch/batch.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
 
 
-if [ ! -d slurms/slurms$1 ]; then
+if [ ! -d gen_slurms/slurms/slurms$1 ]; then
 	echo "Usage: arg1 [0-6]"
 	exit 8
 fi
 
 i=0
-#echo slurms$1
-#for f in slurms/slurms$1/*;
-for f in test/*;
+echo slurms$1
+
+for d in retail kosarak chainstore record;
 do
-	echo $f
-	sbatch $f
-	i=$((i+1))
-	
-	# snooze after submitting 6 batches of exps
-	if [[ $(($i % 6)) -eq 0 ]]; then
-		echo $i "snooze 20s"
-		sleep 20s
-	else	
-		echo $i
-		sleep 3s
-	fi
+	for ((c=0;c<=100000;c+=20000));
+	do
+		for f in gen_slurms/slurms/slurms$1/$d/$c/*;
+		#for f in test/*;
+		do
+			echo $f
+			#sbatch $f
+			i=$((i+1))
+			
+			# snooze after submitting 6 batches of exps
+			if [[ $(($i % 6)) -eq 0 ]]; then
+				echo $i "snooze 120s"
+				sleep 120s
+			else
+				echo $i	
+				if [[ $d == "retail" ]]; then
+					sleep 15s
+				else
+					# 20k is very slow
+					if [ $c -eq 20000 ]; then
+						sleep 120s
+					else
+						sleep 60s
+					fi
+				fi
+			fi
+		done
+	done
 done
 
 echo "$i batches of exps DONE!"
--- auto_sbatch/gen_slurms/gen.sh
+++ auto_sbatch/gen_slurms/gen.sh
@@ -9,6 +9,18 @@ do
 	if [ ! -d slurms/slurms$i ]; then
 		mkdir slurms/slurms$i
 	fi
+	for d in retail kosarak chainstore record;
+	do
+		if [ ! -d slurms/slurms$i/$d ]; then
+			mkdir slurms/slurms$i/$d
+		fi
+		for ((c=0;c<=100000;c+=20000));
+		do
+			if [ ! -d slurms/slurms$i/$d/$c ]; then
+				mkdir slurms/slurms$i/$d/$c
+			fi
+		done
+	done
 done
 
 python gen.py
