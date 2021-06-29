25c186242594c540bffc32a209291161f12b4e4e
jenny011 <jz2915@nyu.edu>
2021-06-30 06:36:30 +0800

    minor

--- auto_sbatch/batch.sh
+++ auto_sbatch/batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if [ ! -d gen_slurms/slurms/slurms$1 ]; then
+if [ ! -d slurms/slurms$1 ]; then
 	echo "Usage: arg1 [0-6]"
 	exit 8
 fi
@@ -13,7 +13,7 @@ for d in retail kosarak chainstore record;
 do
 	for ((c=0;c<=100000;c+=20000));
 	do
-		for f in gen_slurms/slurms/slurms$1/$d/$c/*;
+		for f in slurms/slurms$1/$d/$c/*;
 		#for f in test/*;
 		do
 			echo $f
